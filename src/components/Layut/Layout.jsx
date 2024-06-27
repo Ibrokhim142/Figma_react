2b5253b6d75ea4412c3682d226439923397a4614
Macbook Air <macbookair@MacBook-Air-Macbook.local>
2024-06-27 11:53:28 +0500

    first commit

---
+++ src/components/Layut/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import G from  './Layout.module.css'
+const Layout = () => {
+  return (
+    <div className={G.tittle}>
+            <h3>New Released Games</h3>
+            <div>
+                <div className={G.img}>
+                    <img src={} alt="" />
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default Layout
